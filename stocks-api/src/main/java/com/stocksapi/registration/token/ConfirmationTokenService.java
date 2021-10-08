85143fbf1001dbfda87b2169b956c24c8bafbd8f
unknown <mcena.marco@gmail.com>
2021-10-08 22:06:41 +0800

    registration implementation for confirmation of token

--- stocks-api/src/main/java/com/stocksapi/registration/token/ConfirmationTokenService.java
+++ stocks-api/src/main/java/com/stocksapi/registration/token/ConfirmationTokenService.java
@@ -2,6 +2,9 @@ package com.stocksapi.registration.token;
 
 import org.springframework.stereotype.Service;
 
+import java.time.*;
+import java.util.Optional;
+
 @Service
 public class ConfirmationTokenService {
 	
@@ -14,5 +17,14 @@ public class ConfirmationTokenService {
 	public void saveConfirmationToken(ConfirmationToken confirmationToken) {
 		confirmationTokenRepository.save(confirmationToken);
 	}
+	
+	public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+		
+	public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(
+                token, LocalDateTime.now());
+    }
 	
 }
