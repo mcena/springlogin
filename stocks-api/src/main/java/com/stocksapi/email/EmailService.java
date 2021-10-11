f6f3b08a44e058933a69deedf7c46f96973be6df
unknown <mcena.marco@gmail.com>
2021-10-12 01:20:45 +0800

    removed ununsed imports on EmailService class

--- stocks-api/src/main/java/com/stocksapi/email/EmailService.java
+++ stocks-api/src/main/java/com/stocksapi/email/EmailService.java
@@ -13,7 +13,6 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import com.stocksapi.email.config.EmailProps;
 import com.stocksapi.email.config.IEmailProps;
 
 @Service
